2e0ec812f166452808f987bc786d1468c6df7544
adityaaaaaaaa1407 <adityaaaaaaaa1407>
2024-04-07 11:44:06 +0530

    add component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col  justify-center min-h-screen gap-10 max-container "
+      className="w-full flex xl:flex-row flex-col  justify-center min-h-screen gap-10 m-2 max-container "
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28 ml-5">
         <p className="text-xl font-montserrat text-coral-red">
