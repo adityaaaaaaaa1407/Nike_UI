1cd1d52b982f501a6168e82acff64b5fb998a9d7
adityaaaaaaaa1407 <adityasagavekar14@gmail.com>
2024-04-08 20:17:20 +0530

    add popular product component with card component

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,12 +1,19 @@
 import Hero from "./Hero";
 import Footer from "./Footer";
-import CustomerReviews from './CustomerReviews';
-import PopularProducts from './PopularProducts';
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
 import Subscribe from "./Subscribe";
 import SpecialOffer from "./SpecialOffer";
-import Services from './Services';
-import SuperQuality from './SuperQuality';
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
 
-
-
-export { Hero, Footer, CustomerReviews, PopularProducts, Subscribe, SpecialOffer, Services, SuperQuality }
+export {
+  Hero,
+  Footer,
+  CustomerReviews,
+  PopularProducts,
+  Subscribe,
+  SpecialOffer,
+  Services,
+  SuperQuality,
+};
