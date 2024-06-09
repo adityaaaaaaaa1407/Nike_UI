d0ac23211b6d1ada599dda64f4760758a9ff5b0f
Aditya Sagavekar <adityasagavekar14@gmail.com>
2024-06-09 17:10:51 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,31 +8,34 @@ import { shoes, statistics } from "../constants";
 const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(shoes[0].bigShoe);
   return (
-    <section
-      id="home"
-      className="w-full flex xl:flex-row flex-col  justify-center min-h-screen gap-10 m-2 max-container "
+     <section
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28 ml-5">
-        <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collection is here
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+        <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
             The New Arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3 ">Nike </span> Shoes
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
-        <p className="font-montserrat  text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike Arrivals, quality comfort, and innovation for
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
-        <Button label="Shop Now" iconURL={arrowRight} />
-        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16 ">
-          {statistics.map((stat) => (
-            <div key={stat.label}>
-              <p className="text-4xl font-palanquin font-bold ">{stat.value}</p>
-              <p className="leading-7 font-montserrat text-slate-gray">
+
+        <Button label='Shop now' iconURL={arrowRight} />
+
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((stat, index) => (
+            <div key={index}>
+              <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+              <p className='leading-7 font-montserrat text-slate-gray'>
                 {stat.label}
               </p>
             </div>
@@ -40,19 +43,21 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img
           src={bigShoeImg}
-          alt="big shoe image"
+          alt='shoe colletion'
           width={610}
-          height={500}
-          className="object-contain relative z-10 "
+          height={502}
+          className='object-contain relative z-10'
         />
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-          {shoes.map((shoe) => (
-            <div key={shoe}>
+
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((image, index) => (
+            <div key={index}>
               <ShoeCard
-                imgURL={shoe}
+                index={index}
+                imgURL={image}
                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
               />
